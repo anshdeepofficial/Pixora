daeee70859e8c52f3572c06d5361265914b1afc5
Anshdeep Singh <197048808+anshdeepofficial@users.noreply.github.com>
2026-09-14 21:50:35 +0530

    Enable AI enhanced download flow

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import BatchPairBridge from "./batch-pair-bridge";
+import DownloadEnhancementBridge from "./download-enhancement-bridge";
 import Editor from "./editor";
 
 export default function Home() {
-  return <><BatchPairBridge /><Editor /></>;
+  return <><BatchPairBridge /><DownloadEnhancementBridge /><Editor /></>;
 }
