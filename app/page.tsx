04d2b6c8c689655d5aea9fbd9d3857e8a33fdf7c
Anshdeep Singh <197048808+anshdeepofficial@users.noreply.github.com>
2026-09-14 22:17:15 +0530

    Mount live batch processing progress

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,8 @@
 import BatchPairBridge from "./batch-pair-bridge";
+import BatchProcessingProgress from "./batch-processing-progress";
 import DownloadEnhancementClient from "./download-enhancement-client";
 import Editor from "./editor";
 
 export default function Home() {
-  return <><BatchPairBridge /><DownloadEnhancementClient /><Editor /></>;
+  return <><BatchPairBridge /><BatchProcessingProgress /><DownloadEnhancementClient /><Editor /></>;
 }
