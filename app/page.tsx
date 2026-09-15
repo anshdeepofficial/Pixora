ddeca114505bf73f98ea1b3235695319ccabd0c8
Anshdeep Singh <197048808+anshdeepofficial@users.noreply.github.com>
2026-09-15 22:26:50 +0530

    Mount job recovery and retry client

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,9 @@
 import BatchPairBridge from "./batch-pair-bridge";
+import JobRecoveryClient from "./job-recovery-client";
 import BatchProcessingProgress from "./batch-processing-progress";
 import DownloadEnhancementClient from "./download-enhancement-client";
 import Editor from "./editor";
 
 export default function Home() {
-  return <><BatchPairBridge /><BatchProcessingProgress /><DownloadEnhancementClient /><Editor /></>;
+  return <><BatchPairBridge /><JobRecoveryClient /><BatchProcessingProgress /><DownloadEnhancementClient /><Editor /></>;
 }
