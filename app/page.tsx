9d2f6438176c99218c42c3f3af0695809001e092
Anshdeep Singh <197048808+anshdeepofficial@users.noreply.github.com>
2026-09-17 23:40:22 +0530

    Remove paired batch bridge from Pixora runtime

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,7 @@
-import BatchPairBridge from "./batch-pair-bridge";
 import JobRecoveryClient from "./job-recovery-client";
-import BatchProcessingProgress from "./batch-processing-progress";
 import DownloadEnhancementClient from "./download-enhancement-client";
 import Editor from "./editor";
 
 export default function Home() {
-  return <><BatchPairBridge /><JobRecoveryClient /><BatchProcessingProgress /><DownloadEnhancementClient /><Editor /></>;
+  return <><JobRecoveryClient /><DownloadEnhancementClient /><Editor /></>;
 }
