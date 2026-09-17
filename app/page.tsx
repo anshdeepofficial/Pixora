8edc76eddb05ad3ff6a90b5099e14830f3d1ce8a
Anshdeep Singh <197048808+anshdeepofficial@users.noreply.github.com>
2026-09-17 23:48:01 +0530

    Remove redundant version sync helper

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,7 @@
 import JobRecoveryClient from "./job-recovery-client";
 import DownloadEnhancementClient from "./download-enhancement-client";
-import VersionClient from "./version-client";
 import Editor from "./editor";
 
 export default function Home() {
-  return <><VersionClient /><JobRecoveryClient /><DownloadEnhancementClient /><Editor /></>;
+  return <><JobRecoveryClient /><DownloadEnhancementClient /><Editor /></>;
 }
