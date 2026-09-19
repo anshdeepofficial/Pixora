15a46eaa4895a6ede53ff01e492e19a5ec4b81ab
Anshdeep Singh <197048808+anshdeepofficial@users.noreply.github.com>
2026-09-19 22:20:37 +0530

    Download original quality directly without chooser

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,6 @@
 import JobRecoveryClient from "./job-recovery-client";
-import DownloadEnhancementClient from "./download-enhancement-client";
 import Editor from "./editor";
 
 export default function Home() {
-  return <><JobRecoveryClient /><DownloadEnhancementClient /><Editor /></>;
+  return <><JobRecoveryClient /><Editor /></>;
 }
